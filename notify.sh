69cec933bca5736f69206766f9c0773ddfcd9d44
thelazyindian <satyabrat.me@gmail.com>
2017-08-22 06:43:20 +0000

    Margarita: Travis CI: Add Changelog Support
    * Simply-fy html message
    
    Signed-off-by: Adarsh-MR <adarshmr1998@gmail.com>

--- notify.sh
+++ notify.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 AUTHOR_NAME="$(git log -1 $TRAVIS_COMMIT --pretty="%aN")"
-COMMITTER_EMAIL="$(git log -1 $TRAVIS_COMMIT --pretty="%cE")"
-COMMIT=$(echo ${TRAVIS_COMMIT} | cut -c1-7)
-MSG=I%20will%20now%20build%20${TRAVIS_REPO_SLUG}%20with%20this%20commit%3A%20%3Ca+href%3D%27https%3A%2F%2Fgithub.com%2FCitrus-CAF%2Fpackages_apps_Margarita%2Fcommit%2F${TRAVIS_COMMIT}%27%3E${COMMIT}%3C%2Fa%3E%3A${TRAVIS_COMMIT_MESSAGE}%20by%20${AUTHOR_NAME}
-curl https://api.telegram.org/bot${BOT_TOKEN}/sendMessage -d "chat_id=${CHAT_ID}&text=${MSG}&parse_mode=HTML"
+COMMIT=$(git log -1 $TRAVIS_COMMIT --pretty="%h")
+MSG="I will now build ${TRAVIS_REPO_SLUG} with this commit:
+🔨 <a href=%27https://github.com/${TRAVIS_REPO_SLUG}/commit/${TRAVIS_COMMIT}%27>${COMMIT}</a>: ${TRAVIS_COMMIT_MESSAGE} by ${AUTHOR_NAME}"
+curl https://api.telegram.org/bot${BOT_TOKEN}/sendMessage -d "chat_id=${CHAT_ID}&text=$MSG&parse_mode=HTML"
--- push.sh
+++ push.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 AUTHOR_NAME="$(git log -1 $TRAVIS_COMMIT --pretty="%aN")"
 FILENAME=Margarita-debug-$(date +"%Y%m%d-%H:%M").apk
-COMMIT=$(echo ${TRAVIS_COMMIT} | cut -c1-7)
-MSG=Build%20%23${TRAVIS_BUILD_NUMBER}%20%28%3Ca+href%3D%27https%3A%2F%2Fgithub.com%2FCitrus-CAF%2Fpackages_apps_Margarita%2Fcommit%2F${TRAVIS_COMMIT}%27%3E${COMMIT}%3C%2Fa%3E%29%20of%20${TRAVIS_REPO_SLUG}%40${TRAVIS_BRANCH}%20by%20${AUTHOR_NAME}%20passed.
+COMMIT=$(git log -1 $TRAVIS_COMMIT --pretty="%h")
+MSG="Build <a href=%27https://travis-ci.org/${TRAVIS_REPO_SLUG}/builds/${TRAVIS_BUILD_ID}%27>%23${TRAVIS_BUILD_NUMBER}</a> (<a href=%27https://github.com/${TRAVIS_REPO_SLUG}/commit/${TRAVIS_COMMIT}%27>${COMMIT}</a>) of ${TRAVIS_REPO_SLUG}@${TRAVIS_BRANCH} by ${AUTHOR_NAME} passed."
 mv app/build/outputs/apk/debug/app-debug.apk ${FILENAME}
+CHANGELOG="Changelog:
+$(git log ${TRAVIS_COMMIT_RANGE} --pretty=format:'%h: %s by %aN%n')"
 curl https://slack.com/api/files.upload -F token=${TOKEN} -F channels=#margarita -F title=${FILENAME} -F filename=${FILENAME} -F file=@${FILENAME}
 curl https://api.telegram.org/bot${BOT_TOKEN}/sendDocument -F chat_id=${CHAT_ID} -F document=@${FILENAME}
-curl https://api.telegram.org/bot${BOT_TOKEN}/sendMessage -d "chat_id=${CHAT_ID}&text=${MSG}&parse_mode=HTML"
+curl https://api.telegram.org/bot${BOT_TOKEN}/sendMessage -d "chat_id=${CHAT_ID}&text=$CHANGELOG&parse_mode=HTML"
+curl https://api.telegram.org/bot${BOT_TOKEN}/sendMessage -d "chat_id=${CHAT_ID}&text=$MSG&parse_mode=HTML"
