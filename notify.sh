8ca812ae33c32db2b45f214876537985a2f04cba
Satyabrat Sahoo <satyabrat.me@gmail.com>
2017-08-26 22:15:06 +0530

    Margarita: Fix notify.sh
    * TRAVIS_COMMIT_MESSAGE includes "signed off by name<email>" stuff which gets f***ed when parsed in html format so add COMMIT_MSG variable to store only the necessary shits

--- notify.sh
+++ notify.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 AUTHOR_NAME="$(git log -1 $TRAVIS_COMMIT --pretty="%aN")"
 COMMIT=$(git log -1 $TRAVIS_COMMIT --pretty="%h")
+COMMIT_MSG="$(git log -1 --pretty=format:'%s by %aN')"
 MSG="I will now build ${TRAVIS_REPO_SLUG} with this commit:
-🔨 <a href=%27https://github.com/${TRAVIS_REPO_SLUG}/commit/${TRAVIS_COMMIT}%27>${COMMIT}</a>: ${TRAVIS_COMMIT_MESSAGE} by ${AUTHOR_NAME}"
+🔨 <a href=%27https://github.com/${TRAVIS_REPO_SLUG}/commit/${TRAVIS_COMMIT}%27>${COMMIT}</a>: ${COMMIT_MSG}"
 curl https://api.telegram.org/bot${BOT_TOKEN}/sendMessage -d "chat_id=${CHAT_ID}&text=$MSG&parse_mode=HTML"
