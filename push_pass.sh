25fee44dddfa357e0f423be54c86736678fd4c0e
Adarsh-MR <adarshmr1998@gmail.com>
2017-11-21 19:21:29 +0530

    Margarita: fix apk output on ci builds
    * also fix build warning
    
    Signed-off-by: Adarsh-MR <adarshmr1998@gmail.com>

--- push_pass.sh
+++ push_pass.sh
@@ -3,7 +3,7 @@ AUTHOR_NAME="$(git log -1 $TRAVIS_COMMIT --pretty="%aN")"
 FILENAME=Margarita-debug-$(date +"%Y%m%d-%H:%M").apk
 COMMIT=$(git log -1 $TRAVIS_COMMIT --pretty="%h")
 MSG="Build <a href=%27https://travis-ci.org/${TRAVIS_REPO_SLUG}/builds/${TRAVIS_BUILD_ID}%27>%23${TRAVIS_BUILD_NUMBER}</a> (<a href=%27https://github.com/${TRAVIS_REPO_SLUG}/commit/${TRAVIS_COMMIT}%27>${COMMIT}</a>) of ${TRAVIS_REPO_SLUG}@${TRAVIS_BRANCH} by ${AUTHOR_NAME} passed."
-mv app/build/outputs/apk/debug/app-debug.apk ${FILENAME}
+mv app/build/outputs/apk/debug/Margarita-v*.apk ${FILENAME}
 CHANGELOG="Changelog:
 $(git log ${TRAVIS_COMMIT_RANGE} --pretty=format:'%h: %s by %aN%n')"
 curl https://slack.com/api/files.upload -F token=${TOKEN} -F channels=#margarita -F title=${FILENAME} -F filename=${FILENAME} -F file=@${FILENAME}
