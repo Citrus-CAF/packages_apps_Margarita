4a652138e67bf46de1b0abcfb6c9660e86708ae1
lodlock <iamhunted@gmail.com>
2017-12-04 11:42:07 +0530

    Margarita: fix FAB behaviour with snackbar
    * additional fixes from leonardonogueira
    
    Signed-off-by: Adarsh-MR <adarshmr1998@gmail.com>

---
+++ app/src/main/kotlin/com/citrus/theme/FloatingActionMenuBehavior.java
@@ -0,0 +1,90 @@
+/*
+ * Copyright (C) 2017 Citrus-CAF Project
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package com.citrus.theme;
+
+import android.content.Context;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
+import android.support.v4.view.ViewCompat;
+import android.support.v4.view.ViewPropertyAnimatorListener;
+import android.util.AttributeSet;
+import android.view.View;
+
+import com.github.clans.fab.FloatingActionButton;
+import com.github.clans.fab.FloatingActionMenu;
+
+import java.util.List;
+
+public class FloatingActionMenuBehavior extends CoordinatorLayout.Behavior {
+    private float mTranslationY;
+
+    public FloatingActionMenuBehavior(Context context, AttributeSet attrs) {
+        super();
+    }
+
+    @Override
+    public boolean layoutDependsOn(CoordinatorLayout parent, View child, View dependency) {
+        return dependency instanceof Snackbar.SnackbarLayout;
+    }
+
+    @Override
+    public boolean onDependentViewChanged(CoordinatorLayout parent, View child, View dependency) {
+        if ((child instanceof FloatingActionMenu || child instanceof FloatingActionButton) && dependency instanceof Snackbar.SnackbarLayout) {
+            this.updateTranslation(parent, child, dependency);
+        }
+        return false;
+    }
+
+    @Override
+    public void onDependentViewRemoved(CoordinatorLayout parent, View child, View dependency) {
+        if ((child instanceof FloatingActionMenu || child instanceof FloatingActionButton) && dependency instanceof Snackbar.SnackbarLayout) {
+            this.updateTranslation(parent, child, dependency);
+        }
+    }
+
+    private void updateTranslation(CoordinatorLayout parent, View child, View dependency) {
+        float translationY = this.getTranslationY(parent, child);
+        if (translationY != this.mTranslationY) {
+            ViewCompat.animate(child)
+                    .cancel();
+            if (Math.abs(translationY - this.mTranslationY) == (float) dependency.getHeight()) {
+                ViewCompat.animate(child)
+                        .translationY(translationY)
+                        .setListener((ViewPropertyAnimatorListener) null);
+            } else {
+                ViewCompat.setTranslationY(child, translationY);
+            }
+
+            this.mTranslationY = translationY;
+        }
+
+    }
+
+    private float getTranslationY(CoordinatorLayout parent, View child) {
+        float minOffset = 0.0F;
+        List dependencies = parent.getDependencies(child);
+        int i = 0;
+
+        for (int z = dependencies.size(); i < z; ++i) {
+            View view = (View) dependencies.get(i);
+            if (view instanceof Snackbar.SnackbarLayout && parent.doViewsOverlap(child, view)) {
+                minOffset = Math.min(minOffset, ViewCompat.getTranslationY(view) - (float) view.getHeight());
+            }
+        }
+
+        return minOffset;
+    }
+}
