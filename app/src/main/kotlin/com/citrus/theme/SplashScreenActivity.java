596a971337486e0219e2c9a8945c5d172fe2b4db
Adarsh-MR <adarshmr1998@gmail.com>
2017-12-11 14:07:42 +0530

    Margarita: Add splashscreen
    Signed-off-by: Adarsh-MR <adarshmr1998@gmail.com>

---
+++ app/src/main/kotlin/com/citrus/theme/SplashScreenActivity.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright (C) 2017 Citrus-CAF Project
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package com.citrus.theme;
+
+import android.content.Intent;
+
+import com.daimajia.androidanimations.library.Techniques;
+import com.viksaa.sssplash.lib.activity.AwesomeSplash;
+import com.viksaa.sssplash.lib.cnst.Flags;
+import com.viksaa.sssplash.lib.model.ConfigSplash;
+
+//extends AwesomeSplash!
+public class SplashScreenActivity extends AwesomeSplash {
+
+    @Override
+    public void initSplash(ConfigSplash configSplash) {
+
+        // Animation
+        configSplash.setBackgroundColor(R.color.splash_screen_background); //any color you want form colors.xml
+        configSplash.setAnimCircularRevealDuration(1000); //int ms
+        configSplash.setRevealFlagX(Flags.REVEAL_RIGHT);  //or Flags.REVEAL_LEFT
+        configSplash.setRevealFlagY(Flags.REVEAL_BOTTOM); //or Flags.REVEAL_TOP
+
+        // Logo
+        configSplash.setLogoSplash(R.drawable.splash_screen_logo); //or any other drawable
+        configSplash.setAnimLogoSplashDuration(1000); //int ms
+        configSplash.setAnimLogoSplashTechnique(Techniques.Bounce); //choose one form Techniques (ref: https://github.com/daimajia/AndroidViewAnimations)
+
+        // Title
+        configSplash.setTitleSplash(getString(R.string.splash_screen_app_title));
+        configSplash.setTitleTextColor(R.color.splash_screen_app_title_color);
+        configSplash.setTitleTextSize(30f); //float value
+        configSplash.setAnimTitleDuration(1000);
+        configSplash.setAnimTitleTechnique(Techniques.FadeInUp);
+    }
+
+    @Override
+    public void animationsFinished() {
+        startActivity(new Intent(SplashScreenActivity.this, MainActivity.class));
+        finishAfterTransition();
+    }
+}
