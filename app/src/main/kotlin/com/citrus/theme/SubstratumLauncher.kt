luisbocanegra/substratum-tweaks

Add custom dialog option on theme launch
[harsh@prjkt.io: Rebase to kotlin]
Co-authored-by: Harsh Shandilya <harsh@prjkt.io>
Signed-off-by: Harsh Shandilya <harsh@prjkt.io>

app/src/main/kotlin/substratum/theme/template/SubstratumLauncher.kt
@@ -3,7 +3,6 @@ package substratum.theme.template
 import android.app.Activity
 import android.content.Context
 import android.content.Intent
-import android.content.pm.ApplicationInfo
 import android.os.Bundle
 import android.support.v7.app.AlertDialog
 import android.util.Log
@@ -18,6 +17,8 @@ import substratum.theme.template.Constants.ENABLE_KNOWN_THIRD_PARTY_THEME_MANAGE
 import substratum.theme.template.Constants.ENFORCE_MINIMUM_SUBSTRATUM_VERSION
 import substratum.theme.template.Constants.MINIMUM_SUBSTRATUM_VERSION
 import substratum.theme.template.Constants.OTHER_THEME_SYSTEMS
+import substratum.theme.template.Constants.SHOW_DIALOG_REPEATEDLY
+import substratum.theme.template.Constants.SHOW_LAUNCH_DIALOG
 import substratum.theme.template.Constants.SUBSTRATUM_FILTER_CHECK
 import substratum.theme.template.ThemeFunctions.SUBSTRATUM_PACKAGE_NAME
 import substratum.theme.template.ThemeFunctions.checkSubstratumIntegrity
@@ -30,8 +31,6 @@ import substratum.theme.template.ThemeFunctions.getSubstratumUpdatedResponse
 import substratum.theme.template.ThemeFunctions.hasOtherThemeSystem
 import substratum.theme.template.ThemeFunctions.isCallingPackageAllowed
 import substratum.theme.template.ThemeFunctions.isPackageInstalled
-import java.io.File
-import java.util.*
 
 @Suppress("ConstantConditionIf") // This needs to be defined by the themer, so suppress!
 class SubstratumLauncher : Activity() {
@@ -171,6 +170,10 @@ class SubstratumLauncher : Activity() {
         val intent = intent
         val action = intent.action
         var verified = false
+        val certified = intent.getBooleanExtra("certified", false)
+        val modeLaunch: String? = intent.getStringExtra("theme_mode")
+
+        val sharedPref = getPreferences(Context.MODE_PRIVATE)
         if ((action == substratumIntentData) or (action == getKeysIntent)) {
             verified = when {
                 allowThirdPartySubstratumBuilds() -> true
@@ -190,15 +193,27 @@ class SubstratumLauncher : Activity() {
             Log.d(tag, "'$action' has been authorized to launch this theme.")
         }
 
-        val certified = intent.getBooleanExtra("certified", false)
-        val modeLaunch: String? = intent.getStringExtra("theme_mode")
-
-        val sharedPref = getPreferences(Context.MODE_PRIVATE)
-        if (getInternetCheck()) {
-            if (sharedPref.getInt("last_version", 0) == BuildConfig.VERSION_CODE) {
-                when {
-                    else -> calibrateSystem(certified, modeLaunch)
+        if (SHOW_LAUNCH_DIALOG) run {
+            if (SHOW_DIALOG_REPEATEDLY) {
+                showDialog(certified, modeLaunch)
+                sharedPref.edit().remove("dialog_showed").apply()
+            } else if (!sharedPref.getBoolean("dialog_showed", false)) {
+                showDialog(certified, modeLaunch)
+                sharedPref.edit().putBoolean("dialog_showed", true).apply()
+            } else {
+                if (getInternetCheck()) {
+                    if (sharedPref.getInt("last_version", 0) == BuildConfig.VERSION_CODE) {
+                        calibrateSystem(certified, modeLaunch)
+                    } else {
+                        checkConnection(certified, modeLaunch)
+                    }
+                } else {
+                    calibrateSystem(certified, modeLaunch)
                 }
+            }
+        } else if (getInternetCheck()) {
+            if (sharedPref.getInt("last_version", 0) == BuildConfig.VERSION_CODE) {
+                calibrateSystem(certified, modeLaunch)
             } else {
                 checkConnection(certified, modeLaunch)
             }
@@ -211,6 +226,30 @@ class SubstratumLauncher : Activity() {
         val editor = getPreferences(Context.MODE_PRIVATE).edit()
         editor.putInt("last_version", BuildConfig.VERSION_CODE).apply()
         calibrateSystem(certified, modeLaunch)
+    }
+
+    private fun showDialog(certified: Boolean, modeLaunch: String?) {
+        val dialog = AlertDialog.Builder(this, R.style.DialogStyle)
+                .setCancelable(false)
+                .setIcon(R.mipmap.ic_launcher)
+                .setTitle(R.string.launch_dialog_title)
+                .setMessage(R.string.launch_dialog_content)
+                .setPositiveButton(R.string.launch_dialog_positive) { _, _ ->
+                    val sharedPref = getPreferences(Context.MODE_PRIVATE)
+                    if (getInternetCheck()) {
+                        if (sharedPref.getInt("last_version", 0) == BuildConfig.VERSION_CODE) {
+                            calibrateSystem(certified, modeLaunch)
+                        } else {
+                            checkConnection(certified, modeLaunch)
+                        }
+                    } else {
+                        calibrateSystem(certified, modeLaunch)
+                    }
+                }
+        if (getString(R.string.launch_dialog_negative).isNotEmpty()) {
+            dialog.setNegativeButton(R.string.launch_dialog_negative) { _, _ -> finish() }
+        }
+        dialog.show()
     }
 
     // Load up the JNI library

Citrus-CAF/packages_apps_Margarita

Signed-off-by: Adarsh-MR <adarshmr1998@gmail.com>

app/src/main/kotlin/com/citrus/theme/SubstratumLauncher.kt
@@ -3,7 +3,6 @@ package com.citrus.theme
 import android.app.Activity
 import android.content.Context
 import android.content.Intent
-import android.content.pm.ApplicationInfo
 import android.os.Bundle
 import android.support.v7.app.AlertDialog
 import android.util.Log
@@ -18,6 +17,8 @@ import com.citrus.theme.Constants.ENABLE_KNOWN_THIRD_PARTY_THEME_MANAGERS
 import com.citrus.theme.Constants.ENFORCE_MINIMUM_SUBSTRATUM_VERSION
 import com.citrus.theme.Constants.MINIMUM_SUBSTRATUM_VERSION
 import com.citrus.theme.Constants.OTHER_THEME_SYSTEMS
+import com.citrus.theme.Constants.SHOW_DIALOG_REPEATEDLY
+import com.citrus.theme.Constants.SHOW_LAUNCH_DIALOG
 import com.citrus.theme.Constants.SUBSTRATUM_FILTER_CHECK
 import com.citrus.theme.ThemeFunctions.SUBSTRATUM_PACKAGE_NAME
 import com.citrus.theme.ThemeFunctions.checkSubstratumIntegrity
@@ -30,8 +31,6 @@ import com.citrus.theme.ThemeFunctions.getSubstratumUpdatedResponse
 import com.citrus.theme.ThemeFunctions.hasOtherThemeSystem
 import com.citrus.theme.ThemeFunctions.isCallingPackageAllowed
 import com.citrus.theme.ThemeFunctions.isPackageInstalled
-import java.io.File
-import java.util.*
 
 @Suppress("ConstantConditionIf") // This needs to be defined by the themer, so suppress!
 class SubstratumLauncher : Activity() {
@@ -171,6 +170,10 @@ class SubstratumLauncher : Activity() {
         val intent = intent
         val action = intent.action
         var verified = false
+        val certified = intent.getBooleanExtra("certified", false)
+        val modeLaunch: String? = intent.getStringExtra("theme_mode")
+
+        val sharedPref = getPreferences(Context.MODE_PRIVATE)
         if ((action == substratumIntentData) or (action == getKeysIntent)) {
             verified = when {
                 allowThirdPartySubstratumBuilds() -> true
@@ -190,15 +193,27 @@ class SubstratumLauncher : Activity() {
             Log.d(tag, "'$action' has been authorized to launch this theme.")
         }
 
-        val certified = intent.getBooleanExtra("certified", false)
-        val modeLaunch: String? = intent.getStringExtra("theme_mode")
-
-        val sharedPref = getPreferences(Context.MODE_PRIVATE)
-        if (getInternetCheck()) {
-            if (sharedPref.getInt("last_version", 0) == BuildConfig.VERSION_CODE) {
-                when {
-                    else -> calibrateSystem(certified, modeLaunch)
+        if (SHOW_LAUNCH_DIALOG) run {
+            if (SHOW_DIALOG_REPEATEDLY) {
+                showDialog(certified, modeLaunch)
+                sharedPref.edit().remove("dialog_showed").apply()
+            } else if (!sharedPref.getBoolean("dialog_showed", false)) {
+                showDialog(certified, modeLaunch)
+                sharedPref.edit().putBoolean("dialog_showed", true).apply()
+            } else {
+                if (getInternetCheck()) {
+                    if (sharedPref.getInt("last_version", 0) == BuildConfig.VERSION_CODE) {
+                        calibrateSystem(certified, modeLaunch)
+                    } else {
+                        checkConnection(certified, modeLaunch)
+                    }
+                } else {
+                    calibrateSystem(certified, modeLaunch)
                 }
+            }
+        } else if (getInternetCheck()) {
+            if (sharedPref.getInt("last_version", 0) == BuildConfig.VERSION_CODE) {
+                calibrateSystem(certified, modeLaunch)
             } else {
                 checkConnection(certified, modeLaunch)
             }
@@ -211,6 +226,30 @@ class SubstratumLauncher : Activity() {
         val editor = getPreferences(Context.MODE_PRIVATE).edit()
         editor.putInt("last_version", BuildConfig.VERSION_CODE).apply()
         calibrateSystem(certified, modeLaunch)
+    }
+
+    private fun showDialog(certified: Boolean, modeLaunch: String?) {
+        val dialog = AlertDialog.Builder(this, R.style.DialogStyle)
+                .setCancelable(false)
+                .setIcon(R.mipmap.ic_launcher)
+                .setTitle(R.string.launch_dialog_title)
+                .setMessage(R.string.launch_dialog_content)
+                .setPositiveButton(R.string.launch_dialog_positive) { _, _ ->
+                    val sharedPref = getPreferences(Context.MODE_PRIVATE)
+                    if (getInternetCheck()) {
+                        if (sharedPref.getInt("last_version", 0) == BuildConfig.VERSION_CODE) {
+                            calibrateSystem(certified, modeLaunch)
+                        } else {
+                            checkConnection(certified, modeLaunch)
+                        }
+                    } else {
+                        calibrateSystem(certified, modeLaunch)
+                    }
+                }
+        if (getString(R.string.launch_dialog_negative).isNotEmpty()) {
+            dialog.setNegativeButton(R.string.launch_dialog_negative) { _, _ -> finish() }
+        }
+        dialog.show()
     }
 
     // Load up the JNI library